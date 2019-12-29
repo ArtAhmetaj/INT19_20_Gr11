e43c490d38f1e59a597516c4fdf5c8698e9e0432
ArtAhmetaj <artahmetaj@yahoo.com>
2019-12-29 12:59:36 +0100

    a lot of changes

---
+++ FolderiFinal/HomePage/newone3.js
@@ -0,0 +1,38 @@
+
+      function ndrroje(){
+      var x= document.getElementById("abbr");
+      x.innerHTML="Portali Informatik i Kosoves";
+      }
+      function ktheje()
+{
+      var y = document.getElementById("abbr");
+      y.innerHTML="PIK";
+}
+   
+
+
+
+function kthejengjyren()
+
+{
+   var z = document.getElementsByClassName("item");
+
+var i;
+
+   for (i = 0; i < z.length; i++) { 
+  z[i].style.backgroundColor="#FFE53B";
+  z[i].style.backgroundImage="linear-gradient(118deg, #FFE53B 0%, #FF2525 74%";
+   }
+
+}
+
+
+
+
+    /*
+background: rgb(2,0,36);
+background: 
+
+onclick="setTimeout(gradientcolor(), 3000);"
+
+*/

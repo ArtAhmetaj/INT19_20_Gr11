d98c25a307dab5b48fdd20faa9e2fce0c28af77f
rinorahmeti <rinor.ahmeti@studentet.uni-pr.edu>
2020-01-03 22:54:29 +0100

    transformoje donalld trampin

---
+++ FolderiFinal/Random/metransformo.js
@@ -0,0 +1,6 @@
+function meTransformo()
+{
+    document.getElementById("tramp").style.webkitTransform="rotateY(-111deg)";
+    // document.getElementById("tramp").style.webkitTransform="rotateZ(-11deg)";
+
+}
